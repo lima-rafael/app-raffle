d00584a5345b5ad104c69d689e18a68a52e9e4d6
Rafael <rafa.lima.01@live.com>
2026-06-19 15:57:06 -0300

    rate limited implemented

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -4,27 +4,52 @@ namespace App\Livewire\Auth;
 
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Validation\ValidationException;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Login extends Component
 {
     #[Validate(['required', 'email'])]
-    public string $email = '';
+    public ?string $email = '';
 
     #[Validate(['required', 'string'])]
-    public string $password = '';
+    public ?string $password = '';
 
     public function handle(): void
     {
         $this->validate();
+        $this->ensureIsNotRateLimited();
 
-        if (Auth::attempt([
-            'email' => $this->email,
-            'password' => $this->password,
-        ], true)) {
-            $this->redirectRoute('home');
+        if (!Auth::attempt(['email' => $this->email, 'password' => $this->password,], true)) {
+            throw ValidationException::withMessages([
+                'email' => __('auth.failed'),
+            ]);
         }
+        RateLimiter::clear($this->rateKey());
+        session()->regenerate();
+        $this->redirectRoute('home');
+    }
+
+    private function ensureIsNotRateLimited(): void
+    {
+        if(RateLimiter::tooManyAttempts($this->rateKey(), 5)) {
+            $seconds = RateLimiter::availableIn($this->rateKey());
+            throw ValidationException::withMessages([
+                'email' => 'Too many login attempts. Please try again in ' . $seconds . ' seconds.',
+            ]);
+        }
+
+        RateLimiter::hit($this->rateKey());
+    }
+
+    private function rateKey(): string
+    {
+        return str($this->email . '|' . request()->ip())
+            ->replace('@', '_at_')
+            ->replace('.', '_')
+            ->slug();
     }
 
     public function render(): View
